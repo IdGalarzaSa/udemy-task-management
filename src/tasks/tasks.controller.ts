0fd878a8a4538b4ca8317fb79ddb926583909667
Ivan Galarza <igalarza@bnb.com.bo>
2021-05-08 21:56:01 -0400

    getTaskById refactor

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDTO } from './dtos/create-task.dto';
 import { GetTasksFilterDTO } from './dtos/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskEntity } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService) { }
 
+	@Get('/:id')
+	getTaskByID(@Param('id', ParseIntPipe) id:number): Promise<TaskEntity> {
+	 	return this.tasksService.getTaskById(id);
+	}
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,6 +4,7 @@ import { v1 as uuid} from 'uuid';
 import { CreateTaskDTO } from './dtos/create-task.dto';
 import { GetTasksFilterDTO } from './dtos/get-tasks-filter.dto';
 import { TaskRepository } from './task.repository';
+import { TaskEntity } from './task.entity';
 
 @Injectable()
 export class TasksService {
@@ -13,5 +14,14 @@ export class TasksService {
 		private taskRepository: TaskRepository,
 	){}
 
+	async getTaskById(id: number): Promise<TaskEntity> {
+		const found = await this.taskRepository.findOne(id);
+
+		if (!found) {
+			throw new NotFoundException(`Task with ID ${id} not found.`);
+		}
+
+		return found;
+	}
 	
 }
